21c3d7d3e86f97f9253b47704bf2b356de49267e
Slobodad <44872516+Slobodad@users.noreply.github.com>
2019-03-29 12:10:26 +0200

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -23,10 +23,9 @@ $(document).ready(function () {
 var map;
 var mapConteiner = $('#map')[0];
 var mapPosition1 = { lat: 51.5016188, lng: 31.2924649 };
-// var mapPosition2 = {lat: 51.5028208, lng: 31.2874864};
-// var mapPosition3 = {lat: 51.4935383, lng: 31.2943086};
-// var mapPosition4 = {lat: 51.4961353, lng: 31.2981734};
-// var popupContent = '<p>Что угодно</p>';
+var mapPosition2 = {lat: 51.5028208, lng: 31.2874864};
+var mapPosition3 = {lat: 51.4935383, lng: 31.2943086};
+var mapPosition4 = {lat: 51.4961353, lng: 31.2981734};
 
 function initMap() {
     map = new google.maps.Map(mapConteiner, {
@@ -38,84 +37,82 @@ function initMap() {
     var iconMarker = 'img/marker.png';
     var iconMarkerRed = 'img/marker-red.png';
 
-    var marker = new google.maps.Marker({
+    var marker1 = new google.maps.Marker({
         position: mapPosition1,
         map: map,
         icon: iconMarker
     });
 
-    // marker.id = 'marker-1'
-
-    var contentS = '<div class="popup" id="marker-1">' +
-        '<div class="popup__img">'
-    '<img src="img/image.png" alt="Photo">'
-    '</div>'
-    '<h5 class="popup__title">Новый берег</h5>'
-    '<div class="rating" id="js-rating" data-slide="4">'
-    '<button class="rating__btn" data-rate="1">'
-    '<i class="fas fa-star rating__star rating__star--active"></i>'
-    '</button>'
-    '<button class="rating__btn" data-rate="1">'
-    '<i class="fas fa-star rating__star rating__star--active"></i>'
-    '</button>'
-    '<button class="rating__btn" data-rate="1">'
-    '<i class="fas fa-star rating__star rating__star--active"></i>'
-    '</button>'
-    '<button class="rating__btn" data-rate="1">'
-    '<i class="fas fa-star rating__star rating__star--active"></i>'
-    '</button>'
-    '<button class="rating__btn" data-rate="1">'
-    '<i class="fas fa-star rating__star rating__star--active"></i>'
-    '</button>'
-    '</div>'
-    '<p class="popup__description">'
-    'Жилой комплекс «Новый Берег» имеет 24 этажей (+тех. этаж) и обеспечен подземным паркингом.'
-    '</p>'
-    '<div class="popup__link">'
-    '<a href="#">Подробнее</a>'
-    '</div>'
-    '<div class="popup__logo">'
-    '<img src="img/logo-popup.png" alt="logo">'
-    '</div>'
-    '<button class="popup__close" id="popup-close">'
-    '</button>'
-    '</div>'
+    var content = '<div class="popup" id="marker-1">' +
+        '<div class="popup__img">'+
+    '<img src="img/image.png" alt="Photo">'+
+    '</div>'+
+    '<h5 class="popup__title">Новый берег</h5>'+
+    '<div class="rating" id="js-rating" data-slide="4">'+
+    '<button class="rating__btn" data-rate="1">'+
+    '<i class="fas fa-star rating__star rating__star--active"></i>'+
+    '</button>'+
+    '<button class="rating__btn" data-rate="1">'+
+    '<i class="fas fa-star rating__star rating__star--active"></i>'+
+    '</button>'+
+    '<button class="rating__btn" data-rate="1">'+
+    '<i class="fas fa-star rating__star rating__star--active"></i>'+
+    '</button>'+
+    '<button class="rating__btn" data-rate="1">'+
+    '<i class="fas fa-star rating__star rating__star--active"></i>'+
+    '</button>'+
+    '<button class="rating__btn" data-rate="1">'+
+    '<i class="fas fa-star rating__star rating__star--active"></i>'+
+    '</button>'+
+    '</div>'+
+    '<p class="popup__description">'+
+    'Жилой комплекс «Новый Берег» имеет 24 этажей (+тех. этаж) и обеспечен подземным паркингом.'+
+    '</p>'+
+    '<div class="popup__link">'+
+    '<a href="#">Подробнее</a>'+
+    '</div>'+
+    '<div class="popup__logo">'+
+    '<img src="img/logo-popup.png" alt="logo">'+
+    '</div>'+
+    '<button class="popup__close" id="popup-close">'+
+    '</button>'+
+    '</div>';
 
     var infowindow = new google.maps.InfoWindow({
-        content: contentS
+        content: content
     });
 
-    marker.addListener('click', function () {
-        // marker.classList.toggleClass('popup-active');
-        infowindow.open(map, marker);
+    marker1.addListener('click', function () {
+        infowindow.open(map, marker1);
     });
 
-    // infowindow = new google.maps.InfoWindow({
-    //     content: popupContent
-    // });
+    var marker2 = new google.maps.Marker({
+        position: mapPosition2,
+        map: map,
+        icon: iconMarker    
+    });
 
-    // google.maps.event.addListener(marker1, 'click', function() {
-    //     marker1.info.open(map, marker);
+    marker2.addListener('click', function () {
+        infowindow.open(map, marker2);
+    });
 
-    // marker1.addEventListener('click', function() {
-    //     infowindow.open(map, marker);
-    // });
+    var marker3 = new google.maps.Marker({
+        position: mapPosition3,
+        map: map,
+        icon: iconMarkerRed    
+    });
 
-    // var marker2 = new google.maps.Marker({
-    //     position: mapPosition2,
-    //     map: map,
-    //     icon: iconMarker    
-    // });
+    marker3.addListener('click', function () {
+        infowindow.open(map, marker3);
+    });
 
-    // var marker3 = new google.maps.Marker({
-    //     position: mapPosition3,
-    //     map: map,
-    //     icon: iconMarkerRed    
-    // });
+    var marker4 = new google.maps.Marker({
+        position: mapPosition4,
+        map: map,
+        icon: iconMarker    
+    });
 
-    // var marker4 = new google.maps.Marker({
-    //     position: mapPosition4,
-    //     map: map,
-    //     icon: iconMarker    
-    // });
+    marker4.addListener('click', function () {
+        infowindow.open(map, marker4);
+    });
 }
